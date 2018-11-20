79d9887d3ce6ef5468eccc16ecadb1b617aeb27a
pinklypop@hotmail.com <pinklypop@hotmail.com>
2018-11-20 15:11:17 +0700

    Change 1 file. What -> wat

--- bloodgroup 14.22.51.c
+++ bloodgroup 14.22.51.c
@@ -5,11 +5,11 @@ void main()
   int i;
   do
   {
-    printf("What is your arm? ");
+    printf("Wat is your arm? ");
     scanf("%s", Name);
     do
     {
-      printf("What is your blood group [A, B, AB, or O]? :  ");
+      printf("Wat is your blood group [A, B, AB, or O]? :  ");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
